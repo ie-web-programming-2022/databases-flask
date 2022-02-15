2dce5a5ccc20d940dfd26ea83f315e57a1283cb7
Pepe García <pepe@pepegar.com>
2022-02-16 00:22:50 +0100

    update code from class

--- paymepal.py
+++ paymepal.py
@@ -16,14 +16,24 @@ def login():
     login_query = f"""
     SELECT id, username
     FROM users
-    WHERE username='{username}' and password='{password}'
+    WHERE username='{username}' AND password='{password}'
+    """
+
+    transactions_query = f"""
+    SELECT transactions.amount, transactions.currency, shops.name
+    FROM transactions
+    INNER JOIN users ON transactions.user_id=users.id
+    INNER JOIN shops ON transactions.shop_id=shops.id
+    WHERE username='{username}'
+    ORDER BY transactions.timestamp
     """
 
     with engine.connect() as connection:
         user = connection.execute(login_query).fetchone()
 
         if user:
-            return render_template("private.html", user=user)
+            transactions = connection.execute(transactions_query).fetchall()
+            return render_template("private.html", user=user, transactions=transactions)
         else:
             return render_template("unauthorized.html"), 403
 
